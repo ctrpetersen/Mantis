5dd3b07f471e6a43cb8947969ba9c5edda7f7480
ctrpetersen <ctrpetersen@outlook.com>
2019-11-16 21:07:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mantis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
